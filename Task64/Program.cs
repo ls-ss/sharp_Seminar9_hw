e44842405dab15426edd5f746938ec8ae36cac5e
SergeyL <mkru@mail.ru>
2022-09-17 11:31:41 +0300

    Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -4,12 +4,18 @@ M = 1; N = 5 -> "2, 4"
 M = 4; N = 8 -> "4, 6, 8"                   */
 Console.Clear();
 
-void recursion(int m, int n){
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine());
+
+parityNumRecursion(m, n);
+
+void parityNumRecursion(int m, int n){
     if( m % 2 == 0 ) Console.Write($"{m}, ");
 
-    if     ( n > m ) recursion( m + 1, n );
-    else if( m > n ) recursion( m - 1, n );
-    else               Console.WriteLine();
+    if     ( n > m ) parityNumRecursion( m + 1, n );
+    else if( m > n ) parityNumRecursion( m - 1, n );
+    else             Console.WriteLine();
 }
-
-recursion(-11, 10);
